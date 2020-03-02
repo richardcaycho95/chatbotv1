32a671b125d5582f4fae02fb577d8fc9fe9aa8f4
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-02 13:10:39 -0500

    actualizacion para realizar pedido

--- new.js
+++ new.js
@@ -59,105 +59,41 @@ app.get('/webhook',(req,res)=>{
 
 //handles message events
 function handleMessage(sender_psid,received_message){
-    let responses=[];
-    let response;
+    let responses=[];//array de respuestas a enviar
+    let response; //respuesta en formato json
+
     if (received_message.text) {
-        let data={
-            'buttons':[
-                {'title':'VER MENÚ DEL DIA 🍛','payload':'menu_dia'},
-                {'title':'VER COMPLEMENTOS','payload':'complementos'},
-                {'title':'VER POSTRES 🍰','payload':'postres'}
-            ],
-            'empresa':'Restaurante Sabor Peruano',
-            'descripcion': 'Ahora puedes realizar tus pedidos mediante nuestro asistente virtual 🤖 😉',
-            'img_url':'https://img.mesa247.pe/archivos/inversiones-sp-sabores-peruanos-eirl/sabores-peruanos-miraflores-logo.jpg'
-        }
-        //creando botones
-        let buttons=[]
-        data.buttons.forEach((button)=>{
-            buttons.push({ "type":"postback", "title":button.title, "payload":button.payload })
-        })
-        //creando el saludo
-        response={
-            'text': 'Hola {{user_first_name}}! 😄'
-        }
-        responses.push(response);
-        //creando bloque inicial
-        response = {
-            "attachment":{
-                "type":"template",
-                "payload":{
-                    "template_type":"generic",
-                    "elements":[
-                        {
-                            "title":data.empresa,
-                            "image_url":data.img_url,
-                            "subtitle":data.descripcion,
-                            "buttons":buttons
-                        }
-                    ]
-                }
-              }
-        }
-        responses.push(response);
+        responses.push(getSaludo()) //creando el saludo
+        responses.push(getBloqueInicial()) //creando bloque inicial
     }
     callSendAPI(sender_psid,responses);
 }
 //handles messaging_postback events
 function handlePostback(sender_psid,received_postback){
-    let responses = [];
     const payload=received_postback.payload;
 
-    let data;
+    let responses = [];
     let response;
+    let data;
     let elements;
 
     console.log(`payload postback ${payload}`);
-
+    //parametros del payload
     switch (payload) {
+        case 'home':
+            responses.push(getBloqueInicial)
+            break;
+        case 'realizar_pedido':
+            //primero se pregunta que entrada desea
+            responses.push(getEntradas())
+            break;
         case 'menu_dia':
-            data={
-                'dia': '2 DE MARZO',
-                'entradas':['🍜 CALDO DE GALLINA','🐟 CEVICHE','🍣 ENSALADA DE PALTA'],
-                'segundos':['✅ ESTOFADO DE POLLO CON PAPAS','✅ ARROZ CON PATO','✅ TALLARINES VERDES CON BISTECK'],
-                'texto_accion':'¿Qué deseas realizar?',
-                'botones_accion':[
-                    {'title':'REALIZAR PEDIDO 🏷','payload':'realizar_pedido'},
-                    {'title':'VOLVER AL MENÚ PRINCIPAL 🏠','payload':'home'}
-                ]
-            };
-            let entradas_text='';
-            let segundos_text='';
-            //formato de lista de entradas
-            data.entradas.map((entrada)=>{
-                entradas_text+=entrada+'\n';
-            });
-            entradas_text.replace(',','');
-            //formato de lista de segundos
-            data.segundos.map((segundo)=>{
-                segundos_text+=segundo+'\n';
-            });
-            segundos_text.replace(',','');
-            response={
-                'text': `📌 ESTE ES EL MENÚ DEL DIA DE HOY ${data.dia} \n\nENTRADAS:\n${entradas_text}\nSEGUNDOS:\n${segundos_text}`
-            };
-            responses.push(response);
-            //bloque sobre acciones
-            buttons=[];
-            data.botones_accion.map((button)=>{
-                buttons.push({ "type":"postback", "title":button.title, "payload":button.payload })
+            //creando mensaje donde se detalla el menú del dia y se pregunta sobre la acción a realizar
+            //se debe recorrer el bucle para leer los formatos json
+            let temp=getMenuDia();
+            temp.forEach((response)=>{
+                responses.push(response);
             })
-            response={
-                'attachment':{
-                    "type":"template",
-                    "payload":{
-                      "template_type":"button",
-                      "text":data.texto_accion,
-                      "buttons":buttons
-                    }
-                  }
-            }
-            responses.push(response);
             break;
         case 'complementos':
             data=[
@@ -255,12 +191,97 @@ function callSendAPI(sender_psid,responses){ //response es un array con los mens
     })
 }
 //end
-
+function getSaludo(){
+    return {'text': 'Hola {{user_first_name}}! 😄'}
+}
+function getBloqueInicial(){
+    let data={
+        'buttons':[
+            {'type':'web_url','title':'REALIZAR PEDIDO','url':'https://vizarro.herokuapp.com'},
+            {'type':'postback','title':'VER MENÚ DEL DIA 🍛','payload':'menu_dia'},
+            {'type':'postback','title':'VER COMPLEMENTOS','payload':'complementos'},
+            {'type':'postback','title':'VER POSTRES 🍰','payload':'postres'}
+        ],
+        'empresa':'Restaurante Sabor Peruano',
+        'descripcion': 'Ahora puedes realizar tus pedidos mediante nuestro asistente virtual 🤖 😉',
+        'img_url':'https://img.mesa247.pe/archivos/inversiones-sp-sabores-peruanos-eirl/sabores-peruanos-miraflores-logo.jpg'
+    }
+    //creando botones
+    let buttons=getButtons(data.buttons);
+    return {
+        "attachment":{
+            "type":"template",
+            "payload":{
+                "template_type":"generic",
+                "elements":[
+                    {
+                        "title":data.empresa,
+                        "image_url":data.img_url,
+                        "subtitle":data.descripcion,
+                        "buttons":buttons
+                    }
+                ]
+            }
+        }
+    }
+}
+function getEntradas(){
+    let data=[{'title':'CALDO DE GALLINA',}]
+}
+function getButtons(buttons){//buttons: array que debe tener de forma obligatoria lso campos (type,title,payload)
+    let temp=[];
+    buttons.forEach((button)=>{
+        if (button.type==='web_url') {
+            format={ "type":button.type, "title":button.title, "url":button.url }
+        } else{
+            format={ "type":button.type, "title":button.title, "payload":button.payload }
+        }
+        temp.push(format)
+    })
+    return temp;
+}
+function getMenuDia(){//return array: [0]=> response del menu,[1]=>response de los botones de accción; se debe leer con bucle
+    data={
+        'dia': '2 DE MARZO',
+        'entradas':['🍜 CALDO DE GALLINA','🐟 CEVICHE','🍣 ENSALADA DE PALTA'],
+        'segundos':['✅ ESTOFADO DE POLLO CON PAPAS','✅ ARROZ CON PATO','✅ TALLARINES VERDES CON BISTECK'],
+        'texto_accion':'¿Qué deseas realizar?',
+        'botones_accion':[
+            {'type':'postback','title':'REALIZAR PEDIDO 🏷','payload':'realizar_pedido'},
+            {'type':'postback','title':'VOLVER AL MENÚ PRINCIPAL 🏠','payload':'home'}
+        ]
+    };
+    let entradas_text='';
+    let segundos_text='';
+    //formato de lista de entradas
+    data.entradas.map((entrada)=>{
+        entradas_text+=entrada+'\n';
+    });
+    //formato de lista de segundos
+    data.segundos.map((segundo)=>{
+        segundos_text+=segundo+'\n';
+    });
+    //bloque sobre acciones
+    buttons=getButtons(data.botones_accion);
+    response_menu={'text': `📌 ESTE ES EL MENÚ DEL DIA DE HOY ${data.dia} \n\nENTRADAS:\n${entradas_text}\nSEGUNDOS:\n${segundos_text}`}
+    response_buttons={
+        'attachment':{
+            "type":"template",
+            "payload":{
+              "template_type":"button",
+              "text":data.texto_accion,
+              "buttons":buttons
+            }
+          }
+    }
+    let temp=[response_menu,response_buttons]
+    return temp;
+}
 app.get('/',(req,res)=>{
     res.status(200).send('main page of webhook...');
 });
 
 //lanzamos el webhook
 app.listen(process.env.PORT || 5000,()=>{
-    console.log('servidor webhook iniciado...');
+    console.log(`servidor webhook iniciado en el puerto ${process.env.PORT} ...`);
 })
