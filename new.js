d7a2c3d1db2523fcff3a9bc4cbba0daec1bea6ed
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-01 19:38:36 -0500

    se aaÃñadió las funciones para recibir mensaje y responder

--- new.js
+++ new.js
@@ -1,6 +1,11 @@
 const express= require('express');
 const bodyParser = require('body-parser');
 
+//variables constantes de ambiente
+const SUSBCRIBE_MODE='subscribe';
+const PAGE_ACCESS_TOKEN=process.env.PAGE_ACCESS_TOKEN;
+
+
 const app = express().use(bodyParser.json());
 
 //peticiones post del endpoint para conectar con facebook
@@ -12,6 +17,16 @@ app.post('/webhook',(req,res)=>{
             //recibimos los mensajes y los procesamos
             const webhookEvent = entry.messaging[0];
             console.log(webhookEvent);
+
+            const sender_psid = webhookEvent.sender.id; //id de quien envia el mensaje
+            console.log(`sender PSID: ${sender_psid}`);
+            //validamos si recibimos mensajes
+            if (webhookEvent.message) {
+                handleMessage(sender_psid,webhookEvent.message);
+            } else if(webhookEvent.postback) {
+                handlePostback(sender_psid,webhookEvent.postback);
+            }
+
         });
         res.status(200).send('MENSAJE RECIBIDO DESDE FACEBOOK');
     } else{
@@ -22,13 +37,12 @@ app.post('/webhook',(req,res)=>{
 app.get('/webhook',(req,res)=>{
     console.log('route GET: /webhook');
 
-    const VERIFY_TOKEN='123456';
     const mode = req.query['hub.mode'];
     const token = req.query['hub.verify_token'];
     const challenge = req.query['hub.challenge'];
 
     if(mode && token){
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+        if (mode === SUSBCRIBE_MODE && token === process.env.VERIFICATION_TOKEN) {
             console.log('WEBHOOK VERIFICADO');
             res.status(200).send(challenge);    
         } else{
@@ -39,6 +53,48 @@ app.get('/webhook',(req,res)=>{
         res.sendStatus(404);
     }
 });
+
+//sección de funciones basicas
+
+//handles message events
+function handleMessage(sender_psid,received_message){
+    let response='';
+    if (received_message.text) {
+        response={
+            'text':`Tu mensaje fue: ${receive_message.text}`
+        };
+        callSendAPI(sender_psid,response);
+    }
+}
+//handles messaging_postback events
+function handlePostback(sender_psid,received_message){
+
+}
+//envia mensajes de respuesta a facebook mediante la "send API"
+function callSendAPI(sender_psid,response){
+    const requestBody = {
+        'recipient':{
+            'id': sender_psid
+        },
+        'message': response
+    };
+
+    request({
+        'uri': 'https://graph.facebook.com/v6.0/me/messages',
+        'qs':{
+            'access_token': process.env.PAGE_ACCESS_TOKEN
+        },
+        'method': 'POST',
+        'json': requestBody
+    },(err,res,body)=>{
+        if (!err) {
+            console.log('Mensaje respondido con el bot');
+        } else{
+            console.error('No se puede responder');
+        }
+    })
+}
+//end
 
 app.get('/',(req,res)=>{
     res.status(200).send('main page of webhook...');
