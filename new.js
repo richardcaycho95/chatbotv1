8a60e42e963ea59d5f204d31ead9041d9c7416b3
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-01 23:21:53 -0500

    corrgiendo error

--- new.js
+++ new.js
@@ -104,6 +104,8 @@ function handlePostback(sender_psid,received_postback){
     let response = '';
     const payload=received_postback.payload;
 
+    console.log(`payload postback ${payload}`);
+
     switch (payload) {
         case 'menu_dia':
             response={
@@ -148,45 +150,12 @@ GASEOSAS INCA KOLA Y COCA COLA:
                         "top_element_style": "compact",
                         "elements": [
                             {
-                                "title": "Classic T-Shirt Collection",
-                                "subtitle": "See all our colors",
-                                "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",          
-                                "buttons": [
-                                    {
-                                        "title": "View",
-                                        "type": "web_url",
-                                        "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                                        "messenger_extensions": true,
-                                        "webview_height_ratio": "tall",
-                                        "fallback_url": "https://peterssendreceiveapp.ngrok.io/"            
-                                    }
-                                ]
-                            },
-                            {
-                                "title": "Classic White T-Shirt",
-                                "subtitle": "See all our colors",
-                                "default_action": 
-                                {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                                    "messenger_extensions": false,
-                                    "webview_height_ratio": "tall"
-                                }
-                            },
-                            {
                                 "title": "Classic Blue T-Shirt",
                                 "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
                                 "subtitle": "100% Cotton, 200% Comfortable",
-                                "default_action": {
-                                    "type": "web_url",
-                                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                                    "messenger_extensions": true,
-                                    "webview_height_ratio": "tall",
-                                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
-                                },
                                 "buttons": [
                                     {
-                                        "title": "Shop Now",
+                                        "title": "Comprar ahora",
                                         "type": "web_url",
                                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
                                         "messenger_extensions": true,
@@ -200,7 +169,6 @@ GASEOSAS INCA KOLA Y COCA COLA:
                 }
             };
             break;
-    
         default:
             break;
     }
