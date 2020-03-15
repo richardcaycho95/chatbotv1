9d5b8aeed4c91fc59ed8b7b569678b3bf7a3faf5
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-14 23:06:57 -0500

    se quito el request-promise

--- new.js
+++ new.js
@@ -1,7 +1,6 @@
 const express= require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
-const rp = require('request-promise');
 
 //variables constantes de ambiente
 const SUSBCRIBE_MODE='subscribe';
