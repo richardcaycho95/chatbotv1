96fd2b10b248dd07590a023693ca47eb34e056e6
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-18 19:23:58 -0500

    eliminando cors

--- new.js
+++ new.js
@@ -387,8 +387,8 @@ app.listen(process.env.PORT || 5000,()=>{
     console.log(`servidor webhook iniciado en el puerto ${process.env.PORT} ...`);
 })
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-    next()
-})
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//     //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//     next()
+// })
