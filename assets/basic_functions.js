e7c4b1d9d0dc272c04e7dd2aff1ea04328e0093d
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-28 15:06:23 -0500

    debuging firebase v3

--- assets/basic_functions.js
+++ assets/basic_functions.js
@@ -1,16 +1,22 @@
 module.exports={
   fillInFirebase: function(snapshot){
     console.log(`snapshot: ${JSON.stringify(snapshot.val())}`)
-    console.log(`snapshot en object values: ${JSON.stringify(Object.values(snapshot.val()))}`)
-    let temp=snapshot.val()
     let temp_return=[]
-    for (const prop in temp) {
-      if (temp.hasOwnProperty(prop)) {
-        temp_return.push(temp[prop])
-      }
-    }
-    //temp_return['key']=snapshot.key
-    console.log(`snapshot en array: ${JSON.stringify(temp_return)}`)
+    snapshot.forEach(element => {
+      let item = element.val()
+      item.key=element.key
+      temp_return.push(item)
+    })
+    //return Object.values(snapshot.val())
+    // let temp=snapshot.val()
+    // let temp_return=[]
+    // for (const prop in temp) {
+    //   if (temp.hasOwnProperty(prop)) {
+    //     temp_return.push(temp[prop])
+    //   }
+    // }
+    // //temp_return['key']=snapshot.key
+    // console.log(`snapshot en array: ${JSON.stringify(temp_return)}`)
     return temp_return
   },
   GMAP_API_KEY:'AIzaSyDxIn9qXbWD1lvSzHCiphSNw7_jiPK6obw'
