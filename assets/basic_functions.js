4b4c39c04fce9ab866ca84b7782ee05b8b419752
Juan Vizarro Ramos <gvr.stroke@gmail.com>
2020-03-28 12:02:26 -0500

    correcion de fillinfirebase

--- assets/basic_functions.js
+++ assets/basic_functions.js
@@ -1,5 +1,6 @@
 module.exports={
   fillInFirebase: function(snapshot){
+    console.log(JSON.stringify(snapshot))
     let temp=snapshot.val()
     let temp_return=[]
     for (const prop in temp) {
@@ -7,7 +8,7 @@ module.exports={
         temp_return.push(temp[prop])
       }
     }
-    temp_return['key']=snapshot.key
+    //temp_return['key']=snapshot.key
     return temp_return
   },
   GMAP_API_KEY:'AIzaSyDxIn9qXbWD1lvSzHCiphSNw7_jiPK6obw'
